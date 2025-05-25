c4cd2104c94d7201ca046321e78349d4eb68c32a
AmmarAtGitHub <ammar.shams.eddin@gmail.com>
2025-05-25 17:00:09 +0200

    feat: add the vehicleDAO interface as part of the database layer implementation

--- src/main/java/com/shamseddin/dao/VehicleDAO.java
+++ src/main/java/com/shamseddin/dao/VehicleDAO.java
@@ -1,1 +1,18 @@
+package com.shamseddin.dao;
 
+import java.util.List;
+
+import com.shamseddin.model.Vehicle;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public interface VehicleDAO{
+
+
+    void addVehicle(Vehicle vehicle);
+    void updateVehicle(Vehicle vehicle);
+    void deleteVehicle(Vehicle vehicle);
+    Optional<Vehicle> getVehicle(int id);
+    Optional<Vehicle> getVehicleByVin(String vin);
+    List<Vehicle> getAllVehicles();
+}
